c3bbed91d5d700c3fff121baed5efd64039b95eb
hulklee1 <hulklee@gmail.com>
2025-02-12 16:46:51 +0900

    a

--- myLib/lcd1602.c
+++ myLib/lcd1602.c
@@ -19,7 +19,7 @@ int i2c_init(I2C_HandleTypeDef *p)
 int i2c_scan()
 {
 	if(hi2c == NULL) return;
-	for(int addr=0;addr<128;addr++)
+	for(int addr=0;addr<256;addr++)
 	{
 		if(HAL_I2C_IsDeviceReady(hi2c, addr, 1, 100) == HAL_OK)
 		{
@@ -29,7 +29,7 @@ int i2c_scan()
 		{
 			printf("  .  ");
 		}
-		if((addr % 16) == 0) printf("\r\n");
+		if((addr % 16) == 15) printf("\r\n");
 	}
 }
 
